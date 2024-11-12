5fdceab556309fae0849e6b60d9ce39fff5210a8
priyanshu jangid <priyanshujangid@yahoo.com>
2024-11-12 21:25:23 +0530

    redwood: Set default lang en-US

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,7 +170,7 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 ifeq ($(TW_DEFAULT_LANGUAGE),)
-TW_DEFAULT_LANGUAGE := zh_CN
+TW_DEFAULT_LANGUAGE := en-US
 endif
 TW_DEFAULT_BRIGHTNESS := 200
 TW_STATUS_ICONS_ALIGN := center
